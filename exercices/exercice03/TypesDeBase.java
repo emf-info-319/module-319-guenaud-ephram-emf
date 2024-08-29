9f4bca3ad2033160ca9eced6a9f436ba6a4ec61a
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-08-29 11:23:21 +0200

    Exercice03

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,11 @@
+package exercices.exercice03;
+
+public class TypesDeBase {
+    public static void main(String[] args) {
+        byte age = 16;
+        byte age10 = 26;
+        String nom = "Ephram Guenaud";
+        System.out.println("Je m'appelle " + nom + " et j'ai " + age + "ans");
+        System.out.println("Dans 10ans, j'aurai " + age10 + "ans");
+    }
+}
---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        byte age = 45;
+        char yenSymbol = '\u00A5';
+        boolean estValide = true;
+        String salut = "salut comme tu vas ?";
+        System.out.println("j'ai " + age + "ans " + yenSymbol + " " + estValide + " " + salut);
+        System.out.println("oui et toi?");
+    }
+}

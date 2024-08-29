5fe166dcf3a148e5ff0e64313cea291ee73ff389
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-08-29 16:45:15 +0200

    wsh

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
         char yenSymbol = '\u00A5';
         boolean estValide = true;
         String salut = "salut comme tu vas ?";
-        System.out.println("j'ai " + age + "ans " + yenSymbol + " " + estValide + " " + salut);
-        System.out.println("oui et toi?");
+        System.out.println(salut);
+        System.out.println("salut");
     }
 }

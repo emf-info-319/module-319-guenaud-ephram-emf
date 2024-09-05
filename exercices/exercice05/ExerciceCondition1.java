6411b4e654f0302bee291ef5d43551a415aee307
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-05 09:02:48 +0200

    Exercice05 1

---
+++ exercices/exercice05/ExerciceCondition1.java
@@ -0,0 +1,16 @@
+public class ExerciceCondition1 {
+    public static void main(String[] args) {
+        int valeurATester = 0;
+        System.out.println("Valeur entrée : " + valeurATester);
+        System.out.println("calcule en cours.....");
+        if (valeurATester > 0){
+            System.out.println("Le nombre est positif");
+        }
+        else if (valeurATester < 0) {
+            System.out.println("Le nombre est négatif");
+        }
+        else if (valeurATester == 0) {
+            System.out.println("Le nombre est NUL");
+        }
+    }
+}

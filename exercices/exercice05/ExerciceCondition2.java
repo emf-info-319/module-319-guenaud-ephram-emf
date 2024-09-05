38ef3407f6dddb542d15d46479e2d16dc6854472
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-05 09:27:22 +0200

    Exercice05 2

--- exercices/exercice05/ExerciceCondition2.java
+++ exercices/exercice05/ExerciceCondition2.java
@@ -1,5 +1,11 @@
 public class ExerciceCondition2 {
     public static void main(String[] args) {
-        
+        long valeurATester = 73;
+        if (valeurATester % 2 == 0) {
+            System.out.println("pair");
+        } 
+        else {
+            System.out.println("impair");
+        }
     }
 }

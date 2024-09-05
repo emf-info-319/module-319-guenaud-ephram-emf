52c89b984913b838fdd8518538336a6fd1454347
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-05 09:55:26 +0200

    Exercice05 3 et 4

---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,15 @@
+public class ExerciceCondition3 {
+    public static void main(String[] args) {
+        int valeur1 = -32;
+        int valeur2 = 452;
+        if (valeur1 + valeur2 > 0) {
+            System.out.println("Le résultat est positif");
+        }
+        else if (valeur1 + valeur2 < 0) {
+            System.out.println("Le résultat est positif");
+        }
+        else {
+            System.out.println("Le résultat est négatif");
+        }
+    }
+}

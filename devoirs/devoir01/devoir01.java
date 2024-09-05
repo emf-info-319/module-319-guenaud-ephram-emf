04b6cf9b108d8ea05320f7e2489bda6a18d3fd6c
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-05 10:35:09 +0200

    devoir01 début

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,7 @@
+public class devoir01 {
+    public static void main(String[] args) {
+        byte monAge = 16; //La variable doit être de type byte car un âge ne vas pas être plus haut que 127
+        boolean estMajeur; //La variable doit être de type boolean pour avoir un vais ou faux
+        
+    }
+}

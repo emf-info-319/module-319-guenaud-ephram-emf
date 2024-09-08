65a4d3b5bf9074a5c9440e59eb1924338b5768ee
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-08 17:04:27 +0200

    Devoir 1

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -1,6 +1,6 @@
 public class devoir01 {
     public static void main(String[] args) {
-        byte monAge = 16; //La variable doit être de type byte car un âge ne vas pas être plus haut que 127
+        byte monAge = 64; //La variable doit être de type byte car un âge ne vas pas être plus haut que 127
         boolean estMajeur; //La variable doit être de type boolean pour avoir un vais ou faux
         if (monAge > 20) {
             estMajeur = true;

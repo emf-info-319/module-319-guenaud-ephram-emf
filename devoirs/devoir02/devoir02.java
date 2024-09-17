beadded7c379cb9791712d996b26fc5131d6033d
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-17 12:20:14 +0200

    Devoir02

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -1,15 +1,27 @@
 public class devoir02 {
-    public final static int CAPACITE_RESERVOIR_A = 3;
-    public final static int CAPACITE_RESERVOIR_B = 5;
+    static final int CAPACITE_RESERVOIR_A = 3;
+    static final int CAPACITE_RESERVOIR_B = 5;
+
     public static void main(String[] args) {
-        int reservoirA;
-        int reservoirB;
-        int remplissage = ( int ) ( Math.random() * ( CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B - 0 + 1 ) ) + 0; 
-        System.out.println("Nombre de litre à remplir : " + remplissage);
-        while (remplissage == 0) {
-            System.out.println("Il y a 5 litres à remplir.");
-            remplissage--;
-            System.out.println("Remplissage du réservoir A...");
+        int reservoirA = 0;
+        int reservoirB = 0;
+
+        int remplissage = ( int ) ( Math.random() * (CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B - 0 + 1 ) );
+
+        System.out.println("Il y a " + remplissage + " litres à remplir.");
+
+        while (reservoirA + reservoirB < remplissage) {
+            if (reservoirA < CAPACITE_RESERVOIR_A) {
+                reservoirA++;
+                System.out.println("Remplissage du réservoir A...");
+            } else if (reservoirB < CAPACITE_RESERVOIR_B) {
+                reservoirB++;
+                System.out.println("Remplissage du réservoir B...");
+            }
+
+            System.out.println("Le réservoir A : " + reservoirA + ", le réservoir B : " + reservoirB);
         }
+
+        System.out.println("Remplissage terminé!");
     }
 }

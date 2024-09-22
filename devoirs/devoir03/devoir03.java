9aa788552a3096f9c508af01c7e4b9a469aaf14e
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-22 18:47:03 +0200

    Devoir03 pas fini

---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,9 @@
+public class devoir03 {
+  public static void main(String[] args) {
+    int jour = 07;
+    int mois = 03;
+    int annee = 2024;
+    int jourN = 31 * mois + jour;
+    System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + jourN + " de l'An.");
+  }
+}

74a2e02f1e90fe4dabf2597aa8b8e13f7070adef
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-26 09:10:45 +0200

    Exercice 03 corriger

--- devoirs/devoir03/devoir03.java
+++ devoirs/devoir03/devoir03.java
@@ -1,9 +1,18 @@
 public class devoir03 {
   public static void main(String[] args) {
-    int jour = 07;
-    int mois = 03;
-    int annee = 2024;
-    int jourN = 31 * mois + jour;
-    System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + jourN + " de l'An.");
+    int jour = 21;
+    int mois = 2;
+    int annee = 2024; //declaration de variable
+    int[] tableauMois;
+    tableauMois = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};//tableau pour avoir tout les jours dans un mois de l'annee 
+    int jourtotal = 0;
+    if (annee %  4 == 0 || annee % 400 == 0) { //modulo pour savoir si l'annee est bisextile ou pas
+        tableauMois[2] = 29;
+    }
+    jourtotal = jourtotal + jour;
+    for (int i = 0; i < mois; i++) { //agrémenter de 1 la variable i tant que la variable ne dépasse pas la valeur du mois
+      jourtotal = jourtotal + tableauMois[i];
+    }
+    System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + jourtotal + " de l'annee " + annee);
   }
 }

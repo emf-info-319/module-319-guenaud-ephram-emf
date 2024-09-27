391c43784ac93062a82f076e569957014216f93e
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-27 16:27:52 +0200

    test

--- Test/Test 2/test2.java
+++ Test/Test 2/test2.java
@@ -5,6 +5,7 @@ public class test2 {
         for(int i = 0; i < monTableau.length; i++){
             monTableau[i] = 1;
             System.out.println("valeur de la cellule " + i + " : " + monTableau[i]);
+            System.out.println("hello world");
         }
     }
 }

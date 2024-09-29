ede2f2486b1e06960a03132a19ef80ff8fd40247
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2024-09-29 21:43:44 +0200

    devoir 4 fini+

--- devoirs/devoir04/devoir4.java
+++ devoirs/devoir04/devoir4.java
@@ -3,28 +3,25 @@ package devoirs.devoir04;
 public class devoir4 {
     public static void main(String[] args) {
         int year = 2024;
-        int month = 9;
-        int day = 30;
+        int month = 1;
+        int day = 31;
+        int monthV = month - 1;
         int[] tableauJ = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         String[] tableauM = new String[] { "Janvier", "Fevrier", "Mars", "Avril", "Mais", "Juin", "Juillet", "Aout",
                 "Septembre", "Octobre", "Novembre", "Decembre" };
+        if (year % 4 == 0) {
+            tableauJ[1] = 29;
+        } else {
+        }
         System.out.println("La date entré est le " + day + "." + month + "." + year);
-        int monthV = month - 1;
-        if (year <= 0) {
+        if (year <= 0 || year <= 0) {
             System.out.println("L'année [" + year + "] est hors limites !");
-        } else if (year >= 9999) {
-            System.out.println("L'année [" + year + "] est hors limites !");
-        } else if (month <= 0) {
+        } else if (month <= 0 || month >= 13) {
             System.out.println("Le mois [" + month + "] est hors limites !");
-        } else if (month >= 13) {
-            System.out.println("Le mois [" + month + "] est hors limites !");
-        } else if (day > tableauJ[monthV]) {
-            System.out.println("Le jour [" + day + "] est hors limites !");
-        } else if (day <= 0) {
+        } else if (day > tableauJ[monthV] || day <= 0) {
             System.out.println("Le jour [" + day + "] est hors limites !");
         } else {
             System.out.println("La date formatée est le " + day + " " + tableauM[monthV] + " " + year);
         }
-
     }
 }
